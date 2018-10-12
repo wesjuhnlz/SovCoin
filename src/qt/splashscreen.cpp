0dec7fad3258c2f68e5659f64f51c3b83f0e7eda
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-12 07:42:47 -0400

    Update splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -44,6 +44,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
 
     // define text to place
     QString titleText       = tr("");
+    QString versionText     = QString(tr("Version %1")).arg(QString::fromStdString(FormatFullVersion()));   
     QString copyrightTextSOV   = QChar(0xA9)+QString(" 2018 ").arg(COPYRIGHT_YEAR) + QString(tr("Developed_by_Devilking6105_IamLegion"));
     QString titleAddText    = networkStyle->getTitleAddText();
     // networkstyle.cpp can't (yet) read themes, so we do it here to get the correct Splash-screen
